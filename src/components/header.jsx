780cc4a2a574aa0ff37abeaccf1d4d8b52b29328
MoisesArango13 <67987574+MoisesArango13@users.noreply.github.com>
2023-06-09 18:31:16 -0600

    Update header.jsx
    Actualización de los Headers

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,18 +4,20 @@ import { tokens } from "../theme"
 const Header = ({title, subtitle}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    return <Box mb="30px">
+    return <Box mb="20px">
         <Typography 
-          variant="h2" 
+          variant="h1" 
           color={colors.grey[100]}
           fontWeight="bold"
-          sx = {{ mb: "5px"}}
+          sx = {{ mb: "10px"}}
         >
             {title}
         </Typography>
         <Typography 
           variant="h5"
           color={colors.greenAccent[400]}
+          fontWeight="lighter"
+          sx = {{ mb: "10px"}}
         >
             {subtitle}
         </Typography>
